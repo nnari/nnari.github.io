ebf0412e5557e0ac5fd82892183fbb455df40f46
Tatu Pesonen <pannariiz@gmail.com>
2020-05-16 22:31:54 +0300

    Updates

---
+++ precache-manifest.b8721d419b98219e38d648d8f5d16080.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd923938da405bd15630854615383aae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68001c54fddc33416c6f",
+    "url": "/static/js/2.c7c21ec3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c7c21ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2af784b2e034167ec8a3",
+    "url": "/static/js/main.db340f9d.chunk.js"
+  },
+  {
+    "revision": "6a5556a5811673abe995",
+    "url": "/static/js/runtime-main.adcbadf6.js"
+  }
+]);
--- src/Components/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Footer = () => {
-    return (
-    <footer class="footer bg-black small text-center text-white-50"><div class="container">Copyright © Tatu Pesonen {new Date().getFullYear()}</div></footer>
-    )
-}
